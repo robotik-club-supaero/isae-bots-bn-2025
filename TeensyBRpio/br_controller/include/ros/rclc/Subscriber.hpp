2c0c40f10ff9f92504ea44704e34dfc3ebc9fc76
Bretagne Antonin <bretagne.antonin@free.fr>
2025-05-04 18:43:05 +0200

    Fix: goTo crash on teensy

--- TeensyBRpio/br_controller/include/ros/rclc/Subscriber.hpp
+++ TeensyBRpio/br_controller/include/ros/rclc/Subscriber.hpp
@@ -4,6 +4,7 @@
 #include "ros/rclc/macros.hpp"
 #include "ros/rclc/type_support.hpp"
 
+#include <micro_ros_utilities/type_utilities.h>
 #include <rcl/rcl.h>
 #include <rclc/executor.h>
 #include <rclc/rclc.h>
@@ -47,15 +48,24 @@ class Subscription {
         if (m_subscription) {
             RCCHECK_SOFT(rcl_subscription_fini(m_subscription.get(), m_node.get()));
         }
+        static micro_ros_utilities_memory_conf_t MEMORY_CONF = generateMemoryConf();
+        RCCHECK_SOFT(micro_ros_utilities_destroy_message_memory(type_support_t<T>::get(), m_msg.get(), MEMORY_CONF) ? RCL_RET_OK : RCL_RET_ERROR);
     }
 
   private:
     using MsgT = MessageWrapper<T>;
 
+    static micro_ros_utilities_memory_conf_t generateMemoryConf() {
+        micro_ros_utilities_memory_conf_t conf = {0};
+        conf.max_ros2_type_sequence_capacity = 20;
+        return conf;
+    }
+
     friend class Node;
     Subscription(std::shared_ptr<rcl_node_t> node, rclc_executor_t *executor, const string_t &topic, std::function<void(const T &)> callback)
         : m_node(std::move(node)), m_subscription(std::make_unique<rcl_subscription_t>()), m_msg(std::make_unique<MsgT>(callback)) {
-
+        static micro_ros_utilities_memory_conf_t MEMORY_CONF = generateMemoryConf();
+        RCCHECK_HARD(micro_ros_utilities_create_message_memory(type_support_t<T>::get(), m_msg.get(), MEMORY_CONF) ? RCL_RET_OK : RCL_RET_ERROR);        
         RCCHECK_HARD(rclc_subscription_init_default(m_subscription.get(), m_node.get(), type_support_t<T>::get(), topic.c_str()));
         RCCHECK_HARD(rclc_executor_add_subscription(executor, m_subscription.get(), m_msg.get(),
                                                     (rclc_subscription_callback_t)MessageWrapper<T>::dispatch, ON_NEW_DATA));
