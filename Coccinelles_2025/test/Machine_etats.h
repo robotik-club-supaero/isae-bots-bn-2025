fac8ccaf9ecdd98be90419864b461edb5247ed7e
Lise Duflos <lise.duflos@student.isae-supaero.fr>
2025-05-13 21:27:48 +0200

    enieme test

---
+++ Coccinelles_2025/test/Machine_etats.h
@@ -0,0 +1,69 @@
+
+#ifndef MACHINE_ETATS_H
+#define MACHINE_ETATS_H
+
+#include <Arduino.h>
+#include <Mesure_pos.h>
+#include <Moteur.h>
+#include <Irsensor.h>
+#include <Asserv.h>
+#include <Define_map.h>
+#include "Ultrason.hpp"
+#include "Serv.h"
+#include "define.h"
+
+#define K 1
+#define dt 10
+#define time_global 10000
+#define time_sensor 8000
+
+#define SPEED 25        // Vitesse en cm/s 25 est la vitesse max des moteurs
+#define DISTANCE_MIN 80 // Distance minimale pour éviter un obstacle en mm
+
+class Machine_etats
+{
+    enum Pami_State
+    {
+        INIT,
+        MOVE,
+        STOP,
+        END,
+
+    };
+
+private:
+    Pami_State etat;
+    long m_time;
+    long m_time_global;
+    long m_time_sensor;
+    bool condx_turn ;
+    bool condy_turn ;
+    bool condx_arret ;
+    bool condy_arret ;
+
+public:
+    int tirette = 1; // TODO Etat par défaut de la tirette , CHANGER SI NECESSAIRE
+    
+    float pos_init_x = DEPART_SUPERSTAR_X; // TODO : A MODIFIER en foction de la stratégie
+    float pos_init_y = DEPART_SUPERSTAR_Y;// TODO : A MODIFIER en foction de la stratégie
+    
+    float pos_finit_x = TOURNE_SUPERSTAR_X; // Premier point clef
+    float pos_finit_y = TOURNE_SUPERSTAR_Y;// Premier point clef
+    
+    float pos_x = 0;
+    float pos_y = 0;
+    float angle = 0;
+    int m_minimum_distance = 1000; 
+
+    Asserv *m_p_asserv;
+    Serv *m_p_servo ;
+
+    Mesure_pos *m_p_mesure_pos;
+    Ultrason *m_p_ultrason;
+
+    Machine_etats(Asserv *p_asserv, Mesure_pos *p_mesure_pos, Ultrason *p_ultrason);
+    void setup();
+    void loop();
+};
+
+#endif
--- Coccinelles_2025/test/main.cpp
+++ Coccinelles_2025/test/main.cpp
@@ -1,37 +1,31 @@
-/**
- * @file main.cpp
- * @brief Programme principal , a implementer dans la pami
- * Pour faire du debug , decommenter les lignes suivient d'un #DEBUG
- *
- */
 #include <Moteur.h>
 #include <Arduino.h>
 #include <ESP32Encoder.h>
 #include <Encodeur.h>
 #include <Mesure_pos.h>
-#include <Serv.h>
+
 #include <define.h>
 #include <Asserv.h>
-#include <Ultrason.hpp>
-#include <Machine_etats.h>
+
+
 
 #define IR_PIN 4 //  ne sert à rien pour le moment , sert pour l'interruption
 
 
-Ultrason ultrason = Ultrason(18, 19);
+
 Moteur moteur_d = Moteur(EN_R, IN1_R, IN2_R);
 Moteur moteur_g = Moteur(EN_L, IN1_L, IN2_L);
 Encodeur encoder_R = Encodeur(CLK_R, DT_R);
 Encodeur encoder_L = Encodeur(CLK_L, DT_L);
 Mesure_pos mesure_pos = Mesure_pos(&encoder_R, &encoder_L);
 Asserv asserv = Asserv(&moteur_d, &moteur_g, &mesure_pos);
-Machine_etats machine_etats = Machine_etats(&asserv, &mesure_pos, &ultrason);
+
 
 void setup()
 {
 
   
-  ultrason.setup();
+  
   mesure_pos.setup();
   // Si on veut tester les encodeurs , on les setup
   /*
@@ -39,7 +33,7 @@ void setup()
   encoder_R.setup();
   */
 
-  Serial.begin(9600); // Initialisation de la communication série
+  Serial.begin(115200); // Initialisation de la communication série
   moteur_g.setup();     // Initialisation des moteurs
   moteur_d.setup();
   Serial.println("here");
@@ -49,9 +43,7 @@ void setup()
   moteur_d.set_speed(255);
   */
   asserv.setup();
-  Serial.println("here2");
-  machine_etats.setup();
-  Serial.println("here3");
+
 }
 
 void loop()
@@ -63,12 +55,12 @@ void loop()
   */
 
   
-  ultrason.loop();
+  asserv.asserv_global(25, 25, 0) ;
   mesure_pos.loop();
-  Serial.println("here4");
+ 
   // #DEBUG Si on veut tester les asservissements , on decommente la lige suivante et on commente machine_etats.loop()
   asserv.loop();
-  machine_etats.loop();
-  Serial.println("here5");
+  
+  
   delay(100); // Delay de 100ms entre chaque boucle
 }
