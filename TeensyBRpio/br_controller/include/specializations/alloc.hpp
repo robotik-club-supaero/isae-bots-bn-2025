2180f8830c1a0bc1c6547e80e4037f1deb6abbbc
Bretagne Antonin <bretagne.antonin@free.fr>
2025-05-15 16:32:58 +0200

    Code cleanup

--- TeensyBRpio/br_controller/include/specializations/alloc.hpp
+++ TeensyBRpio/br_controller/include/specializations/alloc.hpp
@@ -1,6 +1,8 @@
 #ifndef _SPEC_ALLOCATOR_HPP_
 #define _SPEC_ALLOCATOR_HPP_
 
+#if defined(ARDUINO)
+
 #include "configuration.hpp"
 #include "stl/PoolAllocator.hpp"
 
@@ -16,5 +18,22 @@ struct allocator_s {
 
 // Allocation failures are reported through the return value directly
 #define MAY_ALLOC [[nodiscard]]
+
+#else
+
+#include "stl/SystemAllocator.hpp"
+
+extern SystemAllocator ALLOCATOR;
+
+template <typename T>
+struct allocator_s {
+    using type = SystemAllocator;
+    static constexpr type get() { return ALLOCATOR; }
+};
+
+// Allocation failures are reported through exceptions
+#define MAY_ALLOC [[maybe_unused]]
+
+#endif
 
 #endif
